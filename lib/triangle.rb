46614e8c0a2bbd439232e9a8aec09ea24ea467e6
Charles Adkins <cpeadkins@gmail.com>
2020-03-29 22:31:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,11 @@
 class Triangle
-  # attr_accessor 
-  
-  def initialize
-  end
+ attr_accessor :side1, :side2, :side3
+    
+    def initialize(side1, side2, side3)
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
+    end
 
   def kind
     # :equilateral, :isosceles, :scalene
